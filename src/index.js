c3e3652ebb2555d58549521551bb5b993e5a0eb0
rourouli <940537122@qq.com>
2020-05-14 15:36:44 +0800

    babel转义ES6语法学习

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-
-import counter from './counter.js';
-import number from './number.js';
-counter();
-number();
-//如果当前模块开启了hot module replacement,接受的number如果发生变化，移除之前的number,number重新执行
-if(module.hot){
-    module.hot.accept('./number',function(){
-        console.log(1)
-        document.body.removeChild(document.getElementById('number'));
-        number();
-    });
-}
+/**如果是在.babelrc文件设置了usage:true, 则需要在入口文件的最顶部导入pollyfill,来兼容低版本浏览器对ES6语法的处理
+ * 或者可以在webpack。config.js中设置entry进行配置
+ * 这种引入方法，会以全局变量的方式注入ES6语法，会污染全局环境，所以在打包类库的时候要用到别的配置方式，不用这种@babel/plugin-transform-runtime
+*/
+//import "@babel/polyfill";
+//require("@babel/polyfill");
+const arr=[
+    new Promise(()=>{}),
+    new Promise(()=>{})
+];
+arr.map(item=>{
+    console.log(item);
+});
