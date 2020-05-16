55376e9acb1aa725762342a9dcb46424ab795e01
rourouli <940537122@qq.com>
2020-05-16 14:11:19 +0800

    笔记更新，代码分割学习

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -3,10 +3,14 @@ const HtmlWebpackPlugin=require('html-webpack-plugin');
 const {CleanWebpackPlugin}=require('clean-webpack-plugin');
 const webpack=require('webpack');
 module.exports={
-    entry:'./src/index.js',
+    //将第三方库和业务逻辑代码分离打包
+    entry:{
+        main:'./src/index.js',
+        //lodash:'./src/lodash.js'
+    },
     output:{
-        filename:'dist.js',//打包后的文件名，默认是main.js
-        path:path.resolve(__dirname,'dist'),//打包后的文件要放在哪，后面是绝对路径，打包后生成dist文件夹
+        filename:'[name].js',//打包后的文件名，默认是main.js
+        path:path.resolve(__dirname,'../dist'),//打包后的文件要放在哪，后面是绝对路径，打包后生成dist文件夹
     },
     module:{
         rules:[
@@ -46,6 +50,38 @@ module.exports={
         new HtmlWebpackPlugin({
             template:'src/index.html'
         }),
-        new CleanWebpackPlugin()
-    ]
+        /**clean-webpack-plugin默认为build是根目录，所以他只能清除根目录下面的目录，我不能清除根目录之外的文件夹，
+         * 所以../dist会报错，具体配置参考https://github.com/johnagan/clean-webpack-plugin,
+         * 如果output.path配置了绝对路径，此处不需要配置，无需传参*/
+        new CleanWebpackPlugin({
+            cleanAfterEveryBuildPatterns: ['dist']
+        })
+    ],
+    optimization:{
+        //具体配置参考SplitChunksPlugin
+        splitChunks:{
+            chunks: 'all',  //代码分割时只对异步代码分割，设置成all，同时配置vendors，同步异步都分割
+            minSize: 30000,  //小于这个字节将不做代码分割
+            //maxSize: 0,  //大于这个字节将做二次分割
+            minChunks: 1,//当模块被用至少多少次时才进行代码分割
+            maxAsyncRequests: 5,//=同时加载的模块数
+            maxInitialRequests: 3,  //入口文件进入的库
+            automaticNameDelimiter: '-',
+            name: true,
+            //同步导入时的配置
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,//node_modules下的库，会打包到vendors组,命名为vendors-[入口文件name].js
+                    priority: -10,  //比如导入的jquery符合这两组配置，然后将会被打包到优先级大的组
+                   // filename:'vendors.js'   //统一配置打包后的name
+                },
+                default: {
+                    //minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true,//比如a里用到了b，a被打包到vendors组,在打包b时，如果b被打包过，将不会重复打包
+                    filename:'common.js'   //统一配置打包后的name
+                }
+            }
+        }
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-/**如果是在.babelrc文件设置了usage:true, 则需要在入口文件的最顶部导入pollyfill,来兼容低版本浏览器对ES6语法的处理
- * 或者可以在webpack。config.js中设置entry进行配置
- * 这种引入方法，会以全局变量的方式注入ES6语法，会污染全局环境，所以在打包类库的时候要用到别的配置方式，不用这种@babel/plugin-transform-runtime
-*/
-/**
- * babel/pollyfill这样的文件，如果没有到处任何文件，在配置了Tree Shaking后，不会导入任何ES6的语法，但是我们又需要这样的文件
- * 所以，需要在package.json中配置"sideEffects":["@babel/polyfill","*css"],将这种没有任何导出的文件添加进来，
- * 如果没有这样的文件，设置成false,以上配置，在遇到css文件和@babel/polyfill的时候将不使用Tree Shaking
- */
-import "@babel/polyfill";
-//require("@babel/polyfill");
-
-/** Tree Shaking
- * 只支持静态方法的引入，因此只支持ESModule方法的引入
- * 此配置会按需引入，比如此处如果不添加这个配置，会将math.js的所有方法引入，配置后用到什么引入什么
- */
-import {add} from './math.js';
-add(1,6);
+function getComponent(){
+    //异步引入lodash库,打包后名称为wendors-lodash.js，可通过修改webpack.common.js的optimization修改名称
+    return import(/*webpackChunkName:"lodash"*/'lodash').then(({default:_})=>{
+        var element=document.createElement('div');
+        element.innerHTML=_.join(['早上好','rourou-lee']);
+        return element;
+    })
+}
+getComponent().then(element=>{
+    document.body.appendChild(element);
+});
+// import _ from 'lodash';
+// var element=document.createElement('div');
+//          element.innerHTML=_.join(['早上好','rourou-lee']);
+//          document.body.appendChild(element);
+// import {add} from './math.js';
+// console.log(add(1,2));
